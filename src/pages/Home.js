a0d24b8e49cbeb4c4c9bee77543286b23e24ff41
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-06-26 11:15:18 +0200

    Add responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,14 +21,17 @@ function Home() {
   };
 
   return (
-    <div className="h-screen relative flex justify-center flex-col m-auto gap-4 overflow-hidden ">
+    <div
+      className="h-screen relative flex justify-center flex-col m-auto gap-4 overflow-auto md:overflow-hidden"
+      style={{ backgroundColor: '#efefef' }}
+    >
       <MealDetails meal={mealData?.meals[0]} />
-      <div className="absolute fixed bottom-0 w-full m-auto flex justify-center">
+      <div className="fixed bottom-0 w-full m-auto flex justify-center">
         <button
           onClick={handleRandomClick}
           className="px-4 py-2 bg-black text-white rounded-t-lg"
         >
-          Random other meal
+          Find another recipe
         </button>
       </div>
     </div>
